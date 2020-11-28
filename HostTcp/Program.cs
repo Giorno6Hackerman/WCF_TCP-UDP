d9c2af52eb8b0979f7c022ff411802128f2a5acf
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-11-28 13:27:40 +0300

    Add files via upload

---
+++ HostTcp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfTcpLib;
+
+namespace HostTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ServiceTcp));
+            host.AddServiceEndpoint(typeof(IServiceTcp), new NetTcpBinding(), "net.tcp://localhost:8001/tcp");
+            host.Open();
+            Console.WriteLine("Press ENTER to stop the service");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
